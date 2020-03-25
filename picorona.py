1649bc4070865b304cc87428373122482ba83852
Scripsi <scripsi@scripsi.uk>
2020-03-25 21:19:05 +0000

    Added background and virus images

---
+++ picorona.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+# This script is run every day by running "crontab -e" and
+# adding the following line:
+#
+#   1 23 * * * /home/pi/brexit.py >/dev/null 2>&1
+
+from PIL import Image, ImageFont, ImageDraw
+from inky import InkyPHAT
+from font_source_sans_pro import SourceSansPro
+from datetime import datetime, timedelta
+import random
+
+print("""Inky pHAT: picorona
+
+Counting the lockdown
+
+""")
+
+inky_display = InkyPHAT("red")
+
+inky_display.set_border(inky_display.BLACK)
+# inky_display.set_rotation(180)
+
+new_cases = 10
+
+# img = Image.new(mode = "P", size = (inky_display.WIDTH,inky_display.HEIGHT), color = inky_display.WHITE))
+img = Image.open("/home/pi/picorona/background.png")
+draw = ImageDraw.Draw(img)
+virus_img = Image.open("/home/pi/picorona/coronavirus.png")
+
+# Add viruses to image
+for virus in range(1, new_cases, 1):
+    x = random.randint(0, 211)
+    y = random.randint(0, 103)
+
+font = ImageFont.truetype(SourceSansPro, 48)
+
+# qmark = "?"
+# qw, qh = font.getsize(qmark)
+
+# for q in range(1, 200, 1):
+#     qx = random.randint(0, inky_display.WIDTH) - qw
+#     qy = random.randint(0, inky_display.HEIGHT) - qh
+#     draw.text((qx,qy), "?", random.randint(0, 2), font)
+
+# draw.ellipse([81,27,131,77], inky_display.BLACK)
+
+#number_text = str(days_to_brexit1)
+#w, h = font.getsize(number_text)
+#rx = (inky_display.WIDTH / 2) - (w / 2) - 3
+#ry = (inky_display.HEIGHT / 2) - (h / 2) - 3
+#rxx = (inky_display.WIDTH / 2) + (w / 2) + 3
+#ryy = (inky_display.HEIGHT / 2) + (h / 2) + 3
+#draw.rectangle((rx,ry,rxx,ryy), inky_display.BLACK)
+#x = (inky_display.WIDTH / 2) - (w / 2)
+#y = (inky_display.HEIGHT / 2) - (h / 2) - 7
+# first_text = str(days_to_brexit1)
+# second_text = ' or ' + str(days_to_brexit2)
+# fw, fh = font.getsize(first_text)
+# draw.text((x,y), first_text, inky_display.WHITE, font)
+# draw.text((x+fw,y), second_text, inky_display.RED, font)
+#draw.text((x,y), number_text, inky_display.WHITE, font)
+
+# Display the logo image
+
+inky_display.set_image(img.rotate(180))
+inky_display.show()
