f8fa5fa5c9f481b25a365577cb4e781429a92404
Scripsi <scripsi@scripsi.uk>
2020-04-05 14:05:08 +0100

    Much more error checking in web scraping code

--- picorona.py
+++ picorona.py
@@ -48,52 +48,86 @@ date_text = datetime.now().strftime("%d/%m")
 # the date of that statistic in "cases_date"
 
 # get web page content
+web_success = False
+parse_success = False
 req = urllib.request.Request(url="https://www.gov.scot/coronavirus-covid-19/")
-f = urllib.request.urlopen(req)
-xhtml = f.read().decode("utf-8")
+# req = urllib.request.Request(url="https://www.gov.scot/")
+try:
+    f = urllib.request.urlopen(req)
+except urllib.error.URLError as e:
+    web_success = False
+    print("Error! Web page access failed - network or remote server failure.")
+else:
+    web_success = True
+    
+    # parse the web page to find the data
+    xhtml = f.read().decode("utf-8")
+    soup = BeautifulSoup(xhtml, "html.parser")
+    cases_date_text = soup.find(string=re.compile("Scottish test numbers:"))
+    if cases_date_text:
+        try:
+            cases_date = datetime.strptime(cases_date_text, "Scottish test numbers: %d %B %Y")
+        except:
+            parse_success = False
+            print("Error! Parsing of cases date failed.")
+        else:
+            region_table = soup.find("table")
+            if region_table:
+                cases_data = pd.read_html(str(region_table), index_col=0)
+                try:
+                    cases_number = int(cases_data[0].at["Lothian",1])
+                except:
+                    parse_success = False
+                    print("Error! Parsing of Lothian cases number failed.")
+                else:
+                    parse_success = True
+                    # Open the time series data from a csv file
+                    lothian_series = pd.read_csv("lothian.csv", header=None, index_col=0, parse_dates=True, squeeze=True)
 
-# parse the web page to find the data
-soup = BeautifulSoup(xhtml, "html.parser")
-cases_date_text = soup.find(string=re.compile("Scottish test numbers:"))
-cases_date = datetime.strptime(cases_date_text, "Scottish test numbers: %d %B %Y")
-region_table = soup.find("table")
-cases_data = pd.read_html(str(region_table), index_col=0)
-cases_number = int(cases_data[0].at["Lothian",1])
+                    # Update the time series data and save it back to the csv file
+                    lothian_series[cases_date] = cases_number
+                    lothian_series.to_csv("lothian.csv", header=False)
 
-# Open the time series data from a csv file
-lothian_series = pd.read_csv("/home/pi/picorona/lothian.csv", header=None, index_col=0, parse_dates=True, squeeze=True)
+                    # Calculate the number of new cases from the difference between the last two days
+                    last_days = lothian_series.tail(2)
+                    new_cases = last_days[1] - last_days[0]
 
-# Update the time series data and save it back to the csv file
-lothian_series[cases_date] = cases_number
-lothian_series.to_csv("/home/pi/picorona/lothian.csv", header=False)
-
-# Calculate the number of new cases from the difference between the last two days
-last_days = lothian_series.tail(2)
-new_cases = last_days[1] - last_days[0]
-
-print(cases_number,"cases of Coronavirus in Lothian up to", cases_date.strftime("%A, %d %B %Y"))
-print(new_cases,"new cases since the previous day")
+                    print(cases_number,"cases of Coronavirus in Lothian up to", cases_date.strftime("%A, %d %B %Y"))
+                    print(new_cases,"new cases since the previous day")
+            else:
+                parse_success = False
+                print("Error! Parsing of cases table failed.")
+    else:
+        parse_success = False
+        print("Error! Parsing of cases date failed.")
 
 # Phew! we've got all the info we need - now to show it on the display
 
 # Set up the display image
 img = Image.open("/home/pi/picorona/background.png")
 draw = ImageDraw.Draw(img)
+font = ImageFont.truetype(FredokaOne, 24)
 virus_bg_source = img.copy()
 virus_bg = virus_bg_source.crop((inky_display.WIDTH-153,inky_display.HEIGHT-74,inky_display.WIDTH,inky_display.HEIGHT))
 virus_img = Image.open("/home/pi/picorona/coronavirus.png")
 virus_mask = Image.open("/home/pi/picorona/coronavirus-mask.png")
 
-# Add viruses to the image
-for virus in range(new_cases):
-    x = random.randint(0, 152)
-    y = random.randint(0, 73)
-    virus_bg.paste(virus_img,(x-10,y-10),virus_mask)
+if web_success:
+    if parse_success:
+        # Add viruses to the image
+        for virus in range(new_cases):
+            x = random.randint(0, 152)
+            y = random.randint(0, 73)
+            virus_bg.paste(virus_img,(x-10,y-10),virus_mask)
+    else:
+        vdraw = ImageDraw.Draw(virus_bg)
+        vdraw.text((5,30), "Parse Err", inky_display.RED, font)
+else:
+    vdraw = ImageDraw.Draw(virus_bg)
+    vdraw.text((5,30), "Netwk Err", inky_display.RED, font)
 
 img.paste(virus_bg,(inky_display.WIDTH-153,inky_display.HEIGHT-74))
 
-font = ImageFont.truetype(FredokaOne, 24)
-# days_on_lockdown_text = str(days_on_lockdown)
 draw.text((10,57), str(days_on_lockdown), inky_display.WHITE, font)
 
 dw, dh = font.getsize(day_text)
