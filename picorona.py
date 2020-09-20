e610969d1ef78f4c5e9bcaf0329bcb97ec892fe5
Scripsi <scripsi@scripsi.uk>
2020-09-20 15:07:33 +0100

    Changed data source to opendata.nhs.scot

--- picorona.py
+++ picorona.py
@@ -47,73 +47,43 @@ date_text = datetime.now().strftime("%d/%m")
 # the date of that statistic in "cases_date"
 
 # get web page content
+# get web page content
 web_success = False
-
-sparql_endpoint = "https://statistics.gov.scot/sparql"
-sparql_query = """
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-
-SELECT ?periodname ?value
-WHERE {
-	?obs <http://purl.org/linked-data/cube#dataSet> <http://statistics.gov.scot/data/coronavirus-covid-19-management-information> .
-	?obs <http://purl.org/linked-data/sdmx/2009/dimension#refArea> <http://statistics.gov.scot/id/statistical-geography/S08000024> .
-    ?obs <http://purl.org/linked-data/sdmx/2009/attribute#unitMeasure> <http://statistics.gov.scot/def/concept/measure-units/testing-cumulative-people-tested-for-covid-19-positive> .
-	?obs <http://purl.org/linked-data/sdmx/2009/dimension#refPeriod> ?perioduri .
-	?obs <http://statistics.gov.scot/def/measure-properties/count> ?value .
-	?perioduri rdfs:label ?periodname .
-}
-"""
-
-sparql = SPARQLWrapper(sparql_endpoint)
-sparql.setQuery(sparql_query)
-sparql.setReturnFormat(CSV)
+parse_success = False
+dailyurl="https://www.opendata.nhs.scot/dataset/b318bddf-a4dc-4262-971f-0ba329e09b87/resource/7fad90e5-6f19-455b-bc07-694a22f8d5dc/download/total_cases_by_hb.csv"
+trendurl="https://www.opendata.nhs.scot/dataset/b318bddf-a4dc-4262-971f-0ba329e09b87/resource/2dd8534b-0a6f-4744-9253-9565d62f96c2/download/trend_hb.csv"
+health_board="S08000024" #Lothian Health Board
 
 try:
-    sparql_results = sparql.query().convert()
+    daily_cases_data=pd.read_csv(dailyurl, sep=",", index_col="HB")
+    trend_cases_data=pd.read_csv(trendurl, sep=",")
 except:
     web_success = False
-    print("Error! API access failed - network or remote server failure.")  
+    print("Error! Web page access failed - network or remote server failure.")
 else:
     web_success = True
-
-    _csv = pd.compat.StringIO(sparql_results.decode('utf-8'))
-    cases_data=pd.read_csv(_csv, sep=",")
-
-    # rename data columns to something more useful
-    cases_data.rename(
-        columns={
-            'periodname' : 'sample_day',
-            'value' : 'cumulative_count'
-        }, inplace=True
-    )
-    # recast date sample_day column to date format
-    cases_data.loc[:,'sample_day'] = pd.to_datetime(cases_data.sample_day)
+    # parse the daily data
+    new_cases = daily_cases_data.loc[health_board,'NewPositive']
+    cases_number = daily_cases_data.loc[health_board,'TotalCases']
+    # recast date column to date format
+    daily_cases_data.loc[:,'Date'] = pd.to_datetime(daily_cases_data.Date,format="%Y%m%d")
+    cases_date = daily_cases_data.loc[health_board,'Date']
+    
+    # parse the trend data
+    # limit it to just the health board we're interested in
+    trend_cases_data.drop(trend_cases_data[trend_cases_data['HB'] != health_board].index, inplace=True)
+    # recast date column to date format
+    trend_cases_data.loc[:,'Date'] = pd.to_datetime(trend_cases_data.Date,format="%Y%m%d")
     # sort data by date
-    cases_data.sort_values(by=['sample_day'], inplace=True)
+    trend_cases_data.sort_values(by=['Date'], inplace=True)
     # then fix the index
-    cases_data.reset_index(drop=True, inplace=True)
-    # recast cumulative_count column safely to numeric format, replace the NaNs with zero and then recast to int
-    cases_data.cumulative_count = pd.to_numeric(cases_data.cumulative_count,errors='coerce')
-    cases_data.loc[:,'cumulative_count'].fillna(0, inplace=True)
-    cases_data.cumulative_count = cases_data.cumulative_count.astype('Int64')
-
-    # create a daily_count column, replace the NaNs with zero and then recast to int
-    cases_data['daily_count'] = cases_data.cumulative_count.diff()
-    cases_data.loc[:,'daily_count'].fillna(0, inplace=True)
-    cases_data.daily_count = cases_data.daily_count.astype('Int64')
-
+    trend_cases_data.reset_index(drop=True, inplace=True)
     # create a rolling 7day mean
-    cases_data['d7_mean'] = cases_data.daily_count.rolling(window=7,min_periods=1).mean()
-
+    trend_cases_data['d7_mean'] = trend_cases_data.DailyPositive.rolling(window=7,min_periods=1).mean()
     # create a rolling 30day max
-    cases_data['d30_max'] = cases_data.daily_count.rolling(window=30,min_periods=1).max()
-    # print(cases_data.describe())
-    # print(cases_data)
-
-    cases_number = cases_data.at[cases_data.last_valid_index(),'cumulative_count']
-    new_cases = cases_data.at[cases_data.last_valid_index(),'daily_count']
-    cases_date = cases_data.at[cases_data.last_valid_index(),'sample_day']
+    trend_cases_data['d30_max'] = trend_cases_data.DailyPositive.rolling(window=30,min_periods=1).max()
     d30_max = cases_data.at[cases_data.last_valid_index(),'d30_max']
+    
     print(cases_number,"cases of Coronavirus in Lothian up to", cases_date.strftime("%A, %d %B %Y"))
     print(new_cases,"new cases since the previous day")
 
@@ -122,7 +92,8 @@ else:
 # Set up the display image
 img = Image.open("/home/pi/picorona/background.png")
 draw = ImageDraw.Draw(img)
-font = ImageFont.truetype(FredokaOne, 24)
+bigfont = ImageFont.truetype(FredokaOne, 24)
+littlefont = ImageFont.truetype(FredokaOne, 12)
 virus_bg_source = img.copy()
 virus_bg = virus_bg_source.crop((inky_display.WIDTH-153,inky_display.HEIGHT-74,inky_display.WIDTH,inky_display.HEIGHT))
 vdraw = ImageDraw.Draw(virus_bg)
@@ -136,25 +107,26 @@ if web_success:
         y = random.randint(0, 73)
         virus_bg.paste(virus_img,(x-10,y-10),virus_mask)
     # Draw trend line
-    v_max = 50
+    v_max = 100
     v_line = []
     for v_day in range(30):
         x = 3 + ((30-v_day) * 5)
-        y = 72 - (cases_data.at[cases_data.last_valid_index()-v_day,'d7_mean'] / v_max) * 70
+        y = 72 - (trend_cases_data.at[trend_cases_data.last_valid_index()-v_day,'d7_mean'] / v_max) * 70
         v_line.append((x,y))
     vdraw.line(v_line,fill=inky_display.WHITE,width=7)
     vdraw.line(v_line,fill=inky_display.RED,width=3) 
 else:
-    vdraw.text((5,25), "Netwk Error", inky_display.RED, font)
+    vdraw.text((5,25), "Netwk Error", inky_display.RED, bigfont)
 
 img.paste(virus_bg,(inky_display.WIDTH-153,inky_display.HEIGHT-74))
 
-draw.text((10,45), str(new_cases), inky_display.WHITE, font)
+draw.text((10,35), cases_date.strftime("%d/%m/%Y"), inky_display.RED, littlefont)
+draw.text((10,45), str(new_cases), inky_display.WHITE, bigfont)
 
-dw, dh = font.getsize(day_text)
+dw, dh = bigfont.getsize(day_text)
 
-draw.text((5,0), day_text, inky_display.WHITE, font)
-draw.text((10+dw,0), date_text, inky_display.WHITE, font)
+draw.text((5,0), day_text, inky_display.WHITE, bigfont)
+draw.text((10+dw,0), date_text, inky_display.WHITE, bigfont)
 
 # For some reason, I've mounted my Raspberry Pi upside-down, so rotate the image!
 inky_display.set_image(img.rotate(180))
